93fab1d8247981f893a3b644e4549d75edc37184
nilguilhen <nilguilhen25@live.com>
2018-05-04 10:57:59 -0300

    deus é top
    foi

--- src/Controller/ContaController.java
+++ src/Controller/ContaController.java
@@ -4,45 +4,22 @@ import Model.Cliente;
 import Model.Concessionaria;
 import Model.ContaEnergia;
 import UI.CadastrarCliente;
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
 import java.util.ArrayList;
->>>>>>> 8f405771ccdd6e3989b204c445dc92442f5a66bb
-=======
-import java.util.HashSet;
-import java.util.Set;
->>>>>>> parent of f94de6e... gerar conta
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class ContaController {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     private ArrayList<ContaEnergia> contaDB = new ArrayList();
     ArrayList<Cliente> arrayCliente = new ArrayList();
     ArrayList<Concessionaria> arrayConce = new ArrayList();
->>>>>>> 8f405771ccdd6e3989b204c445dc92442f5a66bb
-=======
-    private Set<ContaEnergia> contaDB;
->>>>>>> parent of f94de6e... gerar conta
+
     ClienteController cc = new ClienteController();
     ConcessionariaController con = new ConcessionariaController();
     //Cliente cli = new Cliente();
 
     public ContaController() {
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
->>>>>>> 8f405771ccdd6e3989b204c445dc92442f5a66bb
-=======
-        this.contaDB = new HashSet<>();
->>>>>>> parent of f94de6e... gerar conta
     }
 
     public void create(ContaEnergia contaEnergia) throws Exception {
@@ -64,11 +41,6 @@ public class ContaController {
         }
         return resultado;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-*/
-    
-=======
 
     public int achaEstado(ArrayList<Cliente> Cliente, ArrayList<Concessionaria> Concessionaria) {
         int i;
@@ -78,7 +50,5 @@ public class ContaController {
         }
         return i;
     }
->>>>>>> 2214c36858b8c6b66b244a55e90787b90d4e6023
-=======
->>>>>>> parent of f94de6e... gerar conta
+
 }
