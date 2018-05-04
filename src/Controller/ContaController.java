ee68720fd8bf65b134e48c24b0bea7c617acfa3f
Ianfraga <ian-fraga@hotmail.com>
2018-05-04 12:01:32 -0300

    ghjh
    hjj

--- src/Controller/ContaController.java
+++ src/Controller/ContaController.java
@@ -4,21 +4,18 @@ import Model.Cliente;
 import Model.Concessionaria;
 import Model.ContaEnergia;
 import UI.CadastrarCliente;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ContaController {
 
-    private Set<ContaEnergia> contaDB;
+    private ArrayList<ContaEnergia> contaDB = new ArrayList();
     ClienteController cc = new ClienteController();
     Concessionaria con = new Concessionaria();
-    Cliente cli = new Cliente();
+    //Cliente cli = new Cliente();
 
     public ContaController() {
-
-        this.contaDB = new HashSet<>();
     }
 
     public void create(ContaEnergia contaEnergia) throws Exception {
@@ -26,10 +23,14 @@ public class ContaController {
     }
 
     public float geraConta(String cpf, Float kwh) throws Exception {
-
+        
         float resultado = 0;
+        ArrayList<Cliente> arrayCliente = new ArrayList();
+        ArrayList<Concessionaria> arrayConce = new ArrayList();
+        
         try {
-             if(cc.readByCpf(cpf).getEndereco().getEstado().equals(con.getEndereco().getEstado()))
+             arrayCliente = cc.getArray();
+             if(arrayCliente.get(Integer.parseInt(cpf)).getEndereco().getEstado().equals(con.getEndereco().getEstado()) == true)
 
             return resultado =(con.getTarifa() * (kwh));
         }catch (NumberFormatException ex) {
