2214c36858b8c6b66b244a55e90787b90d4e6023
Ianfraga <ian-fraga@hotmail.com>
2018-05-04 13:32:00 -0300

    gfhg
    gh

--- src/Controller/ContaController.java
+++ src/Controller/ContaController.java
@@ -30,13 +30,23 @@ public class ContaController {
         float resultado = 0;
         arrayCliente = cc.getArray();
         arrayConce = con.getArray();
-
+        int i = achaEstado(arrayCliente, arrayConce);
+        
         try {
-            resultado = ((arrayConce.get(Integer.parseInt(cpf)).getTarifa()) * (kwh));
+            resultado = ((arrayConce.get(i).getTarifa()) * (kwh));
             JOptionPane.showMessageDialog(null, resultado);
         } catch (NumberFormatException ex) {
             Logger.getLogger(CadastrarCliente.class.getName()).log(Level.SEVERE, null, ex);
         }
         return resultado;
+    }
+
+    public int achaEstado(ArrayList<Cliente> Cliente, ArrayList<Concessionaria> Concessionaria) {
+        int i;
+        for(i=0;i<=Concessionaria.size();i++){
+            if (Cliente.get(i).getEndereco().getEstado().equals(Concessionaria.get(i).getEndereco().getEstado()))
+            break;
+        }
+        return i;
     }
 }
